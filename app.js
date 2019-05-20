aa8d669832eb501ceb1541169384fc70b24b86c9
Derek Schlieps <derekschlieps@Dereks-iMac.local>
2019-05-20 10:54:48 -0700

    optimize images and add webp format

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("ShreyBeast Server Has Started");
 });
